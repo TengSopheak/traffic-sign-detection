0e096dad413167a463c17f94d7c2ff35c6498cee
TSORPF <Sopheakteng112@gmail.com>
2025-05-01 16:28:36 +0700

    Fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Feature from "/src/components/Feature";
 import Footer from "/src/components/Footer";
+import logo from "../assets/logo.svg";
 
 function Home() {
   return (
     <div className="min-w-[480px]">
       <div className="flex flex-col m-10 text-center items-center justify-top">
-        <img className="h-25 md:h-35 lg:h-40 mb-6" alt="Logo" src="/src/assets/logo.svg" />
+        <img className="h-25 md:h-35 lg:h-40 mb-6" alt="Logo" src={logo} />
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">Traffic Sign Detection</h1>
         <p className="text-lg md:text-xl lg:text-2xl text-center text-gray-700">
           Capture and identify traffic signs using your device's camera with our easy-to-use application.
